0912638faf730ba65db701ae4e8bad4935fbb5f8
JiaPixel <imranshuvo101@gmail.com>
2025-07-31 18:48:22 +0600

    faild

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
-
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import GTMProvider from "./gtm-provider"; // 👈 Import GTM Provider
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,34 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* ✅ Google Tag Manager Script */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-            `,
-          }}
-        />
+       
+
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* ✅ GTM noscript fallback */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
-        {/* ✅ GTM route tracking client logic */}
-        <GTMProvider />
+           
 
         {/* Your layout content */}
         <Navbar />
