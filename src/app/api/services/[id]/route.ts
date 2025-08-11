35c82ece72e8f804a7f3e3cf52eb6cbc124a9e75
JiaPixel <imranshuvo101@gmail.com>
2025-08-11 21:28:23 +0600

    sssss

--- src/app/api/services/[id]/route.ts
+++ src/app/api/services/[id]/route.ts
@@ -2,10 +2,11 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import type { Service } from '@prisma/client';
 
-// Update service by ID
+/* eslint-disable no-console */
+
 export async function PUT(request: Request): Promise<Response> {
   const url = new URL(request.url);
-  const id = url.pathname.split('/').pop() || '';
+  const id = url.pathname.split('/').pop() ?? '';
 
   const body = (await request.json()) as Partial<Service>;
 
@@ -21,10 +22,9 @@ export async function PUT(request: Request): Promise<Response> {
   }
 }
 
-// Delete service by ID
 export async function DELETE(request: Request): Promise<Response> {
   const url = new URL(request.url);
-  const id = url.pathname.split('/').pop() || '';
+  const id = url.pathname.split('/').pop() ?? '';
 
   try {
     await prisma.service.delete({ where: { id } });
